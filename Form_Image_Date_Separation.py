0bd83a94bbc02564d7e7aad47286637bf91e3022
doc-dk <77376259+doc-dk@users.noreply.github.com>
2021-01-13 12:59:49 +0530

    Update Form_Image_Date_Separation.py

--- Form_Image_Date_Separation.py
+++ Form_Image_Date_Separation.py
@@ -1,5 +1,5 @@
-files = ["00000144-PHOTO-2019-07-27-17-29-48.jpg",
-              "00000145-PHOTO-2019-07-27-17-29-53.jpg",
+files = ["00000144-PHOTO-2019-07-27-17-29-48.jpg",          # please dont hard code information. Either created and read a txt/csv file or in this case you need to use package
+              "00000145-PHOTO-2019-07-27-17-29-53.jpg",     # OS to get folder information
               "00000146-PHOTO-2019-07-27-17-30-04.jpg",
               "00000147-PHOTO-2019-07-27-17-30-12.jpg",
               "00000148-PHOTO-2019-07-27-17-30-14.jpg",
@@ -10,7 +10,7 @@ files = ["00000144-PHOTO-2019-07-27-17-29-48.jpg",
 ###############################################################
 substring = "PHOTO"
 
-jpg_image_name = []
+jpg_image_name = []                                         # please see scan rename files for an example of how I have done this.
 not_jpg_image_name = []
 
 
